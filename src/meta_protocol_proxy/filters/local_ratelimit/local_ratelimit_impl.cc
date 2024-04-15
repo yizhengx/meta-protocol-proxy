044ae915bc3517102681c87cd2fa79ec85ac3dfc
yizhengx <yizhengx@bu.edu>
2024-04-15 00:32:32 -0400

    EnableHRTimer then pop

--- src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit_impl.cc
+++ src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit_impl.cc
@@ -33,8 +33,8 @@ void LocalRateLimiterImpl::bufferRequest(DecoderFilterCallbacks* callbacks){
 
 void LocalRateLimiterImpl::onFillTimer() {
   // ENVOY_LOG(warn, "LocalRateLimiterImpl::onFillTimer");
-  queue.pop();
   fill_timer_->enableHRTimer(timer_duration_);
+  queue.pop();
 }
 
 

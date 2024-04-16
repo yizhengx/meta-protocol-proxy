df4577a0baca2961210c4e61f3471e9f4693cfed
yizhengx <yizhengx@bu.edu>
2024-04-16 12:11:38 -0400

    Add logging

--- src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit.cc
+++ src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit.cc
@@ -43,7 +43,7 @@ FilterStatus LocalRateLimit::onMessageDecoded(MetadataSharedPtr, MutationSharedP
     timeout = max(last_timeout, now) + filter_config_->rateLimiter().delay;
   }
   fill_timer_ = filter_config_->dispatcher_.createTimer([this] { onFillTimer(); });
-  ENVOY_LOG(warn, "Schedule at" + std::string(std::chrono::time_point_cast<std::chrono::microseconds>(timeout).time_since_epoch().count()));
+  ENVOY_LOG(warn, "Schedule at" + std::to_string(std::chrono::time_point_cast<std::chrono::microseconds>(timeout).time_since_epoch().count()));
   fill_timer_->enableHRTimer(std::chrono::duration_cast<std::chrono::microseconds>(timeout - now));
   has_buffered = true;
   return FilterStatus::PauseIteration;

7b0f07de1ed555e44a8173b9b3c480cfecf6795f
yizhengx <yizhengx@bu.edu>
2024-04-16 11:29:06 -0400

    Check critical section synchronization

--- src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit.cc
+++ src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit.cc
@@ -51,6 +51,7 @@ FilterStatus LocalRateLimit::onMessageDecoded(MetadataSharedPtr, MutationSharedP
 }
 
 void LocalRateLimit::onFillTimer(){
+    ENVOY_LOG(warn, "onFillTimer");
   callbacks_->dispatcher().post([=]() {
     ENVOY_LOG(warn, std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now()).time_since_epoch().count());
     callbacks_->continueDecoding();

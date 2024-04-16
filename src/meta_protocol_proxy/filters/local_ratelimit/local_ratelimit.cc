631ea1ecf514bcd5796da24fc22de18fbf701f52
yizhengx <yizhengx@bu.edu>
2024-04-16 10:36:26 -0400

    Fix printing release time of each request

--- src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit.cc
+++ src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit.cc
@@ -18,6 +18,8 @@ FilterConfig::FilterConfig(const LocalRateLimitConfig& cfg, Stats::Scope&,
           cfg.token_bucket().max_tokens(), dispatcher,
           cfg)){
             // ENVOY_LOG(warn, "FilterConfig Constructor");
+            ENVOY_LOG(warn, std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now()).time_since_epoch().count());
+
           }
 
 void LocalRateLimit::onDestroy() { cleanup(); }
@@ -49,7 +51,7 @@ FilterStatus LocalRateLimit::onMessageDecoded(MetadataSharedPtr, MutationSharedP
 }
 
 void LocalRateLimit::onFillTimer(){
-  ENVOY_STREAM_LOG(warn, std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now()).time_since_epoch().count(), *callbacks_);
+  ENVOY_LOG(warn, std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now()).time_since_epoch().count());
   callbacks_->dispatcher().post([=]() {
     callbacks_->continueDecoding();
   });

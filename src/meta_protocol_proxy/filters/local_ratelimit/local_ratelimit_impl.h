587a36719c9b64415b3992ac2366adcdb30133b8
yizhengx <yizhengx@bu.edu>
2024-04-17 17:10:43 -0400

    Fix CAS

--- src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit_impl.h
+++ src/meta_protocol_proxy/filters/local_ratelimit/local_ratelimit_impl.h
@@ -41,7 +41,7 @@ public:
   std::chrono::microseconds delay;
 
 private:
-  std::atomic<std::chrono::system_clock> last_timeout(std::chrono::system_clock::now());
+  std::atomic<std::chrono::time_point<std::chrono::system_clock>> last_timeout();
   LocalRateLimitConfig config_;
   // mutable std::mutex mutex_;
   // uint64_t cas;

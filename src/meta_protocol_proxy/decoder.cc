8692dba88bd13da278c9a14362b8c12818aa4151
yizhengx <yizhengx@bu.edu>
2024-06-19 22:39:41 +0800

    Add decoder logging

--- src/meta_protocol_proxy/decoder.cc
+++ src/meta_protocol_proxy/decoder.cc
@@ -11,14 +11,8 @@ namespace MetaProtocolProxy {
 ProtocolState DecoderStateMachine::onDecodeStream(Buffer::Instance& buffer) {
   auto metadata = std::make_shared<MetadataImpl>();
   metadata->setMessageType(messageType_);
-  std::string protocol = "default";
-  if ((Thrift::ThriftCodec*) a = dynamic_cast<Thrift::ThriftCodec*>(&codec_)){
-    protocol = "thrift";
-  } elif ((Memcached::MemcachedCodec*) b = dynamic_cast<Memcached::MemcachedCodec*>(&codec_)){
-    protocol = "memcached";
-  }
   ENVOY_LOG(debug, "meta protocol decoder: start to decode a message, {} bytes available, {} protocol",
-            buffer.length(), protocol);
+            buffer.length(), codec_);
   auto decodeStatus = codec_.decode(buffer, *metadata);
   ENVOY_LOG(debug, "meta protocol decoder finished");
   if (decodeStatus == DecodeStatus::WaitForData) {

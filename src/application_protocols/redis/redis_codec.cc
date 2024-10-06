9e0ad842af2d6dd99a62104c36dfb0d18f763682
yizhengx <yizhengx@bu.edu>
2024-10-06 18:48:34 -0400

    Nit

--- src/application_protocols/redis/redis_codec.cc
+++ src/application_protocols/redis/redis_codec.cc
@@ -147,7 +147,7 @@ std::string RedisCodec::buffer_to_string(Buffer::Instance& buffer, size_t length
 
 
 void RedisCodec::toMetadata(MetaProtocolProxy::Metadata& metadata) {
-  std::cout << "Redis toMetadata: " << buffer_to_string(*origin_msg_, origin_msg_->length()) << std::endl;
+  std::cout << "Redis toMetadata: " << buffer_to_string(metadata.originMessage(), metadata.originMessage()->length()) << std::endl;
   metadata.originMessage().move(*origin_msg_);
 }
 

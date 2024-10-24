6d89e3a827cb759a879abefa3750a82e4bb1cad3
yizhengx <yizhengx@bu.edu>
2024-10-24 02:41:45 -0400

    Nit

--- src/meta_protocol_proxy/route/hash_policy_impl.cc
+++ src/meta_protocol_proxy/route/hash_policy_impl.cc
@@ -8,16 +8,18 @@ namespace Route {
 
 absl::optional<uint64_t> HashPolicyImpl::generateHash(const Metadata& metadata) const {
   absl::optional<uint64_t> hash;
-  absl::InlinedVector<absl::string_view, 1> header_values;
-  header_values.reserve(hash_policy_.size());
-  for (auto it = hash_policy_.begin(); it < hash_policy_.end(); it++) {
-    auto value = metadata.getString(*it);
-    header_values.push_back(metadata.getString(*it));
-    // Ensure generating same hash value for different order header values.
-    // For example, generates the same hash value for {"foo","bar"} and {"bar","foo"}
-    std::sort(header_values.begin(), header_values.end());
-    hash = HashUtil::xxHash64(absl::MakeSpan(header_values));
-  }
+  // absl::InlinedVector<absl::string_view, 1> header_values;
+  // header_values.reserve(hash_policy_.size());
+  // for (auto it = hash_policy_.begin(); it < hash_policy_.end(); it++) {
+  //   auto value = metadata.getString(*it);
+  //   header_values.push_back(metadata.getString(*it));
+  //   // Ensure generating same hash value for different order header values.
+  //   // For example, generates the same hash value for {"foo","bar"} and {"bar","foo"}
+  //   std::sort(header_values.begin(), header_values.end());
+  //   hash = HashUtil::xxHash64(absl::MakeSpan(header_values));
+  // }
+  // generate a random hash value to avoid connection pooling
+  hash = HashUtil::xxHash64(metadata.getRequestId());
   return hash;
 };
 

09f55cf9808d15aa9917fa64209f180414ed553a
yizhengx <yizhengx@bu.edu>
2024-10-23 20:43:37 -0400

    Nit

--- src/application_protocols/mongodb/mongodb_codec.cc
+++ src/application_protocols/mongodb/mongodb_codec.cc
@@ -1,7 +1,7 @@
 #include "mongodb_codec.h"
 #include "envoy/common/exception.h"
 #include "source/common/common/assert.h"
-#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
 #include <bsoncxx/json.hpp>
 #include <bsoncxx/exception/exception.hpp>
 #include <bsoncxx/types.hpp>

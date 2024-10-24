847dfd0125da1cd464f7921ab3f4c4a73dece72c
yizhengx <yizhengx@bu.edu>
2024-10-23 21:01:16 -0400

    Nit

--- src/application_protocols/mongodb/mongodb_codec.cc
+++ src/application_protocols/mongodb/mongodb_codec.cc
@@ -2,7 +2,7 @@
 #include "envoy/common/exception.h"
 #include "source/common/common/assert.h"
 
-#include "builder/stream/document.hpp"
+#include "/bsoncxx/builder/stream/document.hpp"
 namespace Envoy {
 namespace Extensions {
 namespace NetworkFilters {
